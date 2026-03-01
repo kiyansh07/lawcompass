89fd4418548b2d4126504084d9d1044237f59531
kiyansh07 <ka6179234@gmail.com>
2026-03-01 14:26:42 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,17 @@
+const testConfig = {
+testName: "Law Test 1",
+durationMinutes: 60
+};
+
+const questions = [
+{
+question:"Article 14 is related to?",
+options:["Equality","Liberty","Religion","Education"],
+answer:0
+},
+{
+question:"Father of Indian Constitution?",
+options:["Nehru","Ambedkar","Gandhi","Patel"],
+answer:1
+}
+];
